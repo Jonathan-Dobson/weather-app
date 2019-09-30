6836460637600cd53a7e38df76292d0a69dc8f15
fromjdobson <fromjdobson@gmail.com>
2019-09-30 14:41:17 -0600

    move catch all route to bottom of latlng server

--- latlngServer.js
+++ latlngServer.js
@@ -9,9 +9,11 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, ".", "build")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, ".", "build", "index.html"));
-});
+
+
+
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -64,5 +66,9 @@ function closestCity({lat,lng}){
     }
 }
 
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, ".", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
